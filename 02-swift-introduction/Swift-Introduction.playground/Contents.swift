146c6c791418bfffef4fa8dc8a13d7efb4944dbd
alexcomu <alex.comunian@gmail.com>
2016-08-30 16:55:54 +0200

    start with swift introduction

---
+++ 02-swift-introduction/Swift-Introduction.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// INTRODUCTION
+
+// Variables
+
+var message = "Hello, Friend"
+
+var amIHappy = true
+amIHappy = !amIHappy
+
+var feelGood = true
+
+feelGood = amIHappy ? true : false
+
+var bankAccountBalance = 100
+
+var cashMessage = bankAccountBalance > 50 ? "Great I'm rich" : "Oh F**k"
+
+
+// Strings
+
