8afdf826391e77e2f60caa0bcd4cd2cc0eb43948
alexcomu <alex.comunian@gmail.com>
2016-08-31 15:13:46 +0200

    working on first app

---
+++ 03-my-first-app/alexcomuMiraclePill/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  alexcomuMiraclePill
+//
+//  Created by Alex Comunian on 31/08/16.
+//  Copyright © 2016 Hackademy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
