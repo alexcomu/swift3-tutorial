9c3d6364d448aed135e0866dc2db277e6de02ea3
alexcomu <alex.comunian@gmail.com>
2016-09-01 16:12:34 +0200

    first very basic app done

--- 03-my-first-app/alexcomuMiraclePill/ViewController.swift
+++ 03-my-first-app/alexcomuMiraclePill/ViewController.swift
@@ -11,6 +11,11 @@ import UIKit
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     @IBOutlet weak var statePicker: UIPickerView!
     @IBOutlet weak var stateButton: UIButton!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var countryTextField: UITextField!
+    @IBOutlet weak var zipcodeLabel: UILabel!
+    @IBOutlet weak var zipcodeTextField: UITextField!
+    @IBOutlet weak var successView: UIView!
     
     let states = ["SPAIN", "ITALY", "GERMANY", "FRANCE", "PORTUGAL"]
 
@@ -24,9 +29,17 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func changeHiddenProperties(){
+        statePicker.isHidden = !statePicker.isHidden
+        countryLabel.isHidden = !countryLabel.isHidden
+        countryTextField.isHidden = !countryTextField.isHidden
+        zipcodeLabel.isHidden = !zipcodeLabel.isHidden
+        zipcodeTextField.isHidden = !zipcodeTextField.isHidden
+    }
 
     @IBAction func stateButtonPressed(_ sender: AnyObject) {
-        self.statePicker.isHidden = false
+        changeHiddenProperties()
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -43,8 +56,11 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         stateButton.setTitle(states[row], for: UIControlState.normal)
-        self.statePicker.isHidden = true
+        changeHiddenProperties()
     }
 
+    @IBAction func buyButtonPressed(_ sender: AnyObject) {
+        successView.isHidden = false
+    }
 }
 
