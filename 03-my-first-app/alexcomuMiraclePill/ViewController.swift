2f98cbaf3bcec7db650efaa08e4ea73ac1b4fb06
alexcomu <alex.comunian@gmail.com>
2016-09-01 15:51:45 +0200

    Added picker View to select state

--- 03-my-first-app/alexcomuMiraclePill/ViewController.swift
+++ 03-my-first-app/alexcomuMiraclePill/ViewController.swift
@@ -8,10 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var stateButton: UIButton!
+    
+    let states = ["SPAIN", "ITALY", "GERMANY", "FRANCE", "PORTUGAL"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -19,6 +25,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stateButtonPressed(_ sender: AnyObject) {
+        self.statePicker.isHidden = false
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        stateButton.setTitle(states[row], for: UIControlState.normal)
+        self.statePicker.isHidden = true
+    }
 
 }
 
