5483e89385f9d79f18efec2d2fd42d406dc85cb2
alexcomu <alex.comunian@gmail.com>
2016-09-01 16:57:41 +0200

    scrollView example added

--- 04-working-With-UIScrollView/workingWithUIScrollView/ViewController.swift
+++ 04-working-With-UIScrollView/workingWithUIScrollView/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Here we don't have the dimension of the scrollView -> this is why we'll use the viewDidAppear method
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    override func viewDidAppear(_ animated: Bool) {
+        var contentWidth: CGFloat = 0.0
+        // Set the width of the scrollView
+        let scrollWidth = scrollView.frame.size.width
+        
+        // For each image create an ImageView
+        for x in 0...2{
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+            
+            // Set the position of the image
+            var newX: CGFloat = 0.0
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
+            contentWidth += newX
+            // Add the image to the scrollView
+            scrollView.addSubview(imageView)
+            // Set the frame of the imageView
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height/2) - 75, width: 150, height: 150)
+        }
+        // Show images outside from scrollview
+        scrollView.clipsToBounds = false
+        // Set content size of the scrollview
+        scrollView.contentSize = CGSize(width: contentWidth, height: scrollView.frame.size.height)
 
+    }
 
 }
 
