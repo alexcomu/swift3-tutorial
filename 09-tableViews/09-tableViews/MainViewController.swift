5a9afe1bdc044886481a06eee6f348b6df5a328d
alexcomu <alex.comunian@gmail.com>
2016-09-13 15:25:46 +0200

    working on tableViews

---
+++ 09-tableViews/09-tableViews/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  09-tableViews
+//
+//  Created by Alex Comunian on 13/09/16.
+//  Copyright © 2016 Hackademy. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
---
+++ 09-tableViews/09-tableViews/VideoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VideoViewController.swift
+//  09-tableViews
+//
+//  Created by Alex Comunian on 13/09/16.
+//  Copyright © 2016 Hackademy. All rights reserved.
+//
+
+import UIKit
+
+class VideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
