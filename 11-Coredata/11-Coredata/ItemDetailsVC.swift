cce333d10aa46666ef550e8bdfe3a80c69bfaad9
alexcomu <alex.comunian@gmail.com>
2017-01-18 17:49:43 +0100

    pickerView

--- 11-Coredata/11-Coredata/ItemDetailsVC.swift
+++ 11-Coredata/11-Coredata/ItemDetailsVC.swift
@@ -7,14 +7,85 @@
 //
 
 import UIKit
+import CoreData
 
-class ItemDetailsVC: UIViewController {
 
+class ItemDetailsVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
+
+    @IBOutlet weak var storePicker: UIPickerView!
+    @IBOutlet weak var titleField: CustomTextField!
+    @IBOutlet weak var priceField: CustomTextField!
+    @IBOutlet weak var detailsField: CustomTextField!
+    
+    var stores = [Store]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         if let topItem = self.navigationController?.navigationBar.topItem{
             topItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
         }
+        
+        storePicker.delegate = self
+        storePicker.dataSource = self
+        
+        //generateStores()
+        getStore()
     }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let store = stores[row]
+        return store.name
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return stores.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        // Update at click
+    }
+    
+    func getStore(){
+        
+        let fetchRequest: NSFetchRequest<Store> = Store.fetchRequest()
+        do{
+            self.stores = try context.fetch(fetchRequest)
+            self.storePicker.reloadAllComponents()
+            
+        }catch{
+            // handle error
+            
+        }
+    }
+    
+    func generateStores(){
+        let store = Store(context: context)
+        store.name = "Comu Enterprise"
+        
+        let store2 = Store(context: context)
+        store2.name = "Comu Logic"
+        
+        let store3 = Store(context: context)
+        store3.name = "Comu Shop"
+        
+        let store4 = Store(context: context)
+        store4.name = "Comu Electronics"
+        
+        let store5 = Store(context: context)
+        store5.name = "Amazomu"
+                
+        ad.saveContext()
+    }
+    
+    
+    
+    
+    
+    
+    
 
 }
