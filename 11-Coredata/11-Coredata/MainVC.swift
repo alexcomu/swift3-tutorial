e5d5aa069fb84f462493a9c09bd79cf7af9caec1
alexcomu <alex.comunian@gmail.com>
2017-01-18 10:53:33 +0100

    added timestamp on item and added some style to the view

--- 11-Coredata/11-Coredata/MainVC.swift
+++ 11-Coredata/11-Coredata/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  mainVC
 //  11-Coredata
 //
 //  Created by Alex Comunian on 17/01/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
