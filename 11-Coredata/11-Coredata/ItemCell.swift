a81f9bdccdaea2c1f3adf860865741360a8897fc
alexcomu <alex.comunian@gmail.com>
2017-01-18 11:00:20 +0100

    added itemCell and table view delegates

---
+++ 11-Coredata/11-Coredata/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  11-Coredata
+//
+//  Created by Alex Comunian on 18/01/17.
+//  Copyright © 2017 Alex Comunian. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+
+}
--- 11-Coredata/11-Coredata/MainVC.swift
+++ 11-Coredata/11-Coredata/MainVC.swift
@@ -8,18 +8,29 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segment: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
 
 }
 
