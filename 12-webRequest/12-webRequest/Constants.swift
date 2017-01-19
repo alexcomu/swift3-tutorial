de7ec2e47652fa48375c709c5ff009a1b86bfb66
alexcomu <alex.comunian@gmail.com>
2017-01-19 18:12:25 +0100

    some utils

---
+++ 12-webRequest/12-webRequest/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  12-webRequest
+//
+//  Created by Alex Comunian on 19/01/17.
+//  Copyright © 2017 Alex Comunian. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+let LATITUDE = "lat="
+let LONGITUDE = "&lon="
+let APP_ID = "&appid"
+let API_KEY = "INSERT HERE YOUR API KEY"
+let API_KEY_2 = "b1b15e88fa797225412429c1c50c122a1"
+
+let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)\(LONGITUDE)\(APP_ID)\(API_KEY)"
+
---
+++ 12-webRequest/12-webRequest/CurrentWeather.swift
@@ -0,0 +1,54 @@
+//
+//  CurrentWeather.swift
+//  12-webRequest
+//
+//  Created by Alex Comunian on 19/01/17.
+//  Copyright © 2017 Alex Comunian. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class CurrentWeather{
+    
+    private var _cityName: String!
+    private var _date: String!
+    private var _weatherType: String!
+    private var _currentTemp: Double!
+    
+    var cityName: String {
+        if _cityName == nil{
+            _cityName = ""
+        }
+        return _cityName
+    }
+    
+    var date: String {
+        if _date == nil{
+            _date = ""
+        }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        let currentDate = dateFormatter.string(from: Date())
+        _date = "Today, \(currentDate)"
+        return _date
+    }
+    
+    var weatherType: String {
+        if _weatherType == nil{
+            _weatherType = ""
+        }
+        return _weatherType
+    }
+    
+    var currentTemp: Double {
+        if _currentTemp == nil{
+            _currentTemp = 0.0
+        }
+        return _currentTemp
+    }
+    
+}
+
+
