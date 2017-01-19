c5f0999af4ba905c67b0ec07803ac3351b1a58bb
alexcomu <alex.comunian@gmail.com>
2017-01-19 17:37:20 +0100

    working on layout

---
+++ 12-webRequest/12-webRequest/MainVC.swift
@@ -0,0 +1,30 @@
+//
+//  MainVC.swift
+//  12-webRequest
+//
+//  Created by Alex Comunian on 19/01/17.
+//  Copyright © 2017 Alex Comunian. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
+
