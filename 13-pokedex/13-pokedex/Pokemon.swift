84296e5173396d17f748a3d1d27b6e08473ea36e
alexcomu <alex.comunian@gmail.com>
2017-03-07 11:00:53 +0100

    starting with model

---
+++ 13-pokedex/13-pokedex/Pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  Pokemon.swift
+//  13-pokedex
+//
+//  Created by Alex Comunian on 07/03/17.
+//  Copyright © 2017 Hackademy. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
--- 13-pokedex/13-pokedex/ViewController.swift
+++ 13-pokedex/13-pokedex/ViewController.swift
@@ -12,14 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        let charmender = Pokemon(name: "Charmender", pokedexId: 4)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
